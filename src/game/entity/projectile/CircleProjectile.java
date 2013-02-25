01e74831044487c9a9021ff797dd6bc9deb4be23
Parranoia <alecx315@aim.com>
2013-02-25 12:11:56 -0500

    Implemented Projectiles.

---
+++ src/game/entity/projectile/CircleProjectile.java
@@ -0,0 +1,31 @@
+package game.entity.projectile;
+
+import game.Game;
+import game.entity.Entity;
+import game.entity.Entity.Direction;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+
+public class CircleProjectile extends Projectiles {
+
+	public CircleProjectile(int x, int y, Game game, Entity owner, Direction direction, int size, int power, int speed) {
+		super(x, y, game, owner, direction);
+		
+		width = size;
+		height = size;
+		this.power = power;
+		this.speed = speed;
+	}
+
+	public void render(Graphics2D g) {
+		g.setColor(Color.RED);
+		g.fillOval(getX(), getY(), width, height);
+	}
+
+	public void update(double diff) {
+		move(direction);
+		checkCollision();
+	}
+
+}
