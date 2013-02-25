ea6a9e1875d315a8ed41e4d1ca85cdc7d903c4ac
Parranoia <alecx315@aim.com>
2013-02-25 12:14:36 -0500

    Updated how the update ticks are handled as well as adding a way to get/remove entities and projectiles

--- src/game/Game.java
+++ src/game/Game.java
@@ -1,18 +1,23 @@
 package game;
 
+import game.entity.Enemy;
+import game.entity.Entity;
 import game.entity.Player;
-import game.input.InputHandler;
+import game.entity.projectile.Projectiles;
+import game.graphics.Level;
+import game.handlers.InputHandler;
 import game.logging.Log;
 
 import java.awt.BorderLayout;
 import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 import java.awt.image.BufferStrategy;
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferInt;
+import java.util.ArrayList;
 
 import javax.swing.JFrame;
 
@@ -26,7 +31,10 @@ public class Game extends Canvas implements Runnable {
 	
 	private JFrame frame;
 	private Player player;
+	private Level level;
 	private InputHandler input;
+	private ArrayList<Projectiles> projectiles;
+	private ArrayList<Entity> entities;
 	
 	private boolean isRunning;
 	private boolean debug = true;
@@ -49,11 +57,37 @@ public class Game extends Canvas implements Runnable {
 		frame.setVisible(true);
 		frame.setAlwaysOnTop(true);
 		
-		player = new Player(0, 0);
+		projectiles = new ArrayList<Projectiles>();
+		entities = new ArrayList<Entity>();
+		level = new Level();
+		player = new Player(0, 0, this);
+		entities.add(new Enemy(500, 500, this, player, level));
+		entities.add(player);
+		
 		input = new InputHandler(this, player);
 		player.setInput(input);
 		
 		start();
+	}
+	
+	public void addProjectile(Projectiles p) {
+		projectiles.add(p);
+	}
+	
+	public void removeProjectile(Projectiles p) {
+		projectiles.remove(p);
+	}
+	
+	public void addEntity(Entity e) {
+		entities.add(e);
+	}
+	
+	public void removeEntity(Entity e) {
+		entities.remove(e);
+	}
+	
+	public ArrayList<Entity> getEntities() {
+		return entities;
 	}
 	
 	public void start() {
@@ -69,24 +103,27 @@ public class Game extends Canvas implements Runnable {
 		long last = System.nanoTime();
 		double ns = 1000000000.0/60.0;
 		double timeDiff = 0;
-		
+				
 		while(isRunning) {
 			long now = System.nanoTime();
 			timeDiff += (now - last) / ns;
 			last = now;
-			
+	
 			while (timeDiff >= 1) {
-				update();
+				update(timeDiff);
 				timeDiff--;
 			}
-			
+						
 			paint();
 		}
 	}
 	
-	public void update() {
-		player.update();
-	}
+	public void update(double diff) {
+		for (int i = 0; i < entities.size(); i++)
+			entities.get(i).update(diff);
+		for (int i = 0; i < projectiles.size(); i++)
+			projectiles.get(i).update(diff);		
+		}
 	
 	public void paint() {
 		BufferStrategy bs = getBufferStrategy();
@@ -96,12 +133,18 @@ public class Game extends Canvas implements Runnable {
 		}
 		
 		Graphics2D g = (Graphics2D)bs.getDrawGraphics();
-		g.clearRect(0, 0, getWidth(), getHeight());
+		g.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,
+		        RenderingHints.VALUE_TEXT_ANTIALIAS_GASP);
+		g.clearRect(0, 0, getWidth(), getHeight());	
 		
-		g.fillRect(player.getX(), player.getY(), Player.HEIGHT, Player.WIDTH);
+		for (int i = 0; i < entities.size(); i++)
+			entities.get(i).render(g);
+		for (int i = 0; i < projectiles.size(); i++)
+			projectiles.get(i).render(g);
+		
 		if (debug) {
 			g.setColor(Color.RED);
-			Log.debug(g, player.getX() + ", " + player.getY(), 5, 15);
+			Log.debug(g, player.getX() + ", " + player.getY(), 5, HEIGHT*SCALE - 10);
 		}
 		
 		g.dispose();
