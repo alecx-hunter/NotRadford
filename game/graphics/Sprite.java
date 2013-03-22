ef8d8f0e0ff0602cf321e7d53f1ee56001ec4f97
Alecx <alecx315@aim.cim>
2013-03-22 14:12:06 -0400

    Sprite class will control loading sprites into the game.

---
+++ game/graphics/Sprite.java
@@ -0,0 +1,11 @@
+package game.graphics;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: alecx
+ * Date: 3/22/13
+ * Time: 2:10 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Sprite {
+}
